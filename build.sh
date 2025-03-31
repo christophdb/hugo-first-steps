ffac773729738bfc9fd3aee8e5bc250a9fa12534
Simon Hammes <simonhammes@users.noreply.github.com>
2025-03-31 15:12:03 +0200

    Install dependencies
    node_modules might be empty if the host volume is mounted

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker compose run --rm hugo bash -c "npm run --prefix /hugo/src/themes/nike build && hugo --source /hugo/src"
+docker compose run --rm hugo bash -c "npm install --prefix /hugo/src/themes/nike && npm run --prefix /hugo/src/themes/nike build && hugo --source /hugo/src"
