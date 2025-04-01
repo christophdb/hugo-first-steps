b98d4d9a1fee4ae439145dcc82f3102f5ad4be86
Simon Hammes <simonhammes@users.noreply.github.com>
2025-04-01 08:55:18 +0200

    Send form data to backend

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
-docker compose run --rm hugo bash -c "npm install --prefix /hugo/src/themes/nike && npm run --prefix /hugo/src/themes/nike build && hugo --source /hugo/src"
+# TODO: Production URL
+HUGO_BACKEND_URL="https://api.seatable.com"
+
+docker compose run --rm -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" hugo bash -c "npm install --prefix /hugo/src/themes/nike && npm run --prefix /hugo/src/themes/nike build && hugo --source /hugo/src"
