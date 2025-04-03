f274c02705216aa75e500ae6eb52c492d2af166e
Simon Hammes <simonhammes@users.noreply.github.com>
2025-04-03 13:29:47 +0200

    build.sh: Set HUGO_ENVIRONMENT to production

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 # TODO: Production URL
 HUGO_BACKEND_URL="https://api.seatable.com"
 
-docker compose run --rm -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" hugo bash -c "npm install --prefix /hugo/src/themes/seatable && npm run --prefix /hugo/src/themes/seatable build && hugo --source /hugo/src"
+docker compose run --rm -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" -e HUGO_ENVIRONMENT=production hugo bash -c "npm install --prefix /hugo/src/themes/seatable && npm run --prefix /hugo/src/themes/seatable build && hugo --source /hugo/src"
