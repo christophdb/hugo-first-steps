eb2dcc4583bee73e36e80f70c35dae8a0b67e151
Simon Hammes <simonhammes@users.noreply.github.com>
2025-05-05 17:30:26 +0200

    Highlight active element in TOC

---
+++ src/themes/seatable/assets/js/toc-highlight.js
@@ -0,0 +1,51 @@
+// Based on https://dakotaleemartinez.com/tutorials/how-to-add-active-highlight-to-table-of-contents/
+class Scroller {
+    static init() {
+        if (document.querySelector('#TableOfContents')) {
+            this.tocLinks = document.querySelectorAll('#TableOfContents a');
+            console.log(this.tocLinks);
+            this.tocLinks.forEach((link) => link.classList.add('transition', 'duration-200'));
+            this.headers = Array.from(this.tocLinks).map((link) => {
+                const name = link.href.split('#')[1];
+                return document.querySelector(`[id='${name}']`);
+            });
+            this.ticking = false;
+            window.addEventListener('scroll', (e) => {
+                this.onScroll();
+            });
+        }
+    }
+
+    static onScroll() {
+        if (!this.ticking) {
+            requestAnimationFrame(this.update.bind(this));
+            this.ticking = true;
+        }
+    }
+
+    static update() {
+        this.activeHeader ||= this.headers[0];
+        let activeIndex = this.headers.findIndex((header) => {
+            return header.getBoundingClientRect().top > 180;
+        });
+
+        console.log(activeIndex);
+
+        if (activeIndex == -1) {
+            activeIndex = this.headers.length - 1;
+        } else if (activeIndex > 0) {
+            activeIndex--;
+        }
+        let active = this.headers[activeIndex];
+        if (active !== this.activeHeader) {
+            this.activeHeader = active;
+            this.tocLinks.forEach((link) => link.classList.remove('text-active'));
+            this.tocLinks[activeIndex].classList.add('text-active');
+        }
+        this.ticking = false;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    Scroller.init();
+});
