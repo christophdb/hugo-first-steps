b2b5a5760d745547b69b9b8f11de3226b283f0e2
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2025-05-20 12:14:51 +0200

    fix workflow

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ HUGO_BACKEND_URL="https://forms.seatable.com"
 docker compose run --rm \
     -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" \
     -e HUGO_ENVIRONMENT=production \
-    hugo bash -c "npm run --prefix /hugo/src/themes/seatable build && npm run --prefix /hugo/src/themes/seatable pagefind:watch && hugo --enableGitInfo --source /hugo/src"
+    hugo bash -c "npm run --prefix /hugo/src/themes/seatable build && hugo --enableGitInfo --source /hugo/src && npm run --prefix /hugo/src/themes/seatable pagefind:watch"
