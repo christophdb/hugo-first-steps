927c55d4aaa46250a07e2693070913fcae175d96
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2025-05-20 12:08:04 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ docker compose run --rm \
     --user "$(id -u):$(id -g)" \
     -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" \
     -e HUGO_ENVIRONMENT=production \
-    hugo bash -c "rm -rf /hugo/src/themes/seatable/node_modules /hugo/src/themes/seatable/package-lock.json && npm install --prefix /hugo/src/themes/seatable && npm run --prefix /hugo/src/themes/seatable build && hugo --enableGitInfo --source /hugo/src"
+    hugo bash -c "npm run --prefix /hugo/src/themes/seatable build && hugo --enableGitInfo --source /hugo/src"
