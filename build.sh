fb630aeaa1b853e09728fec4ae8736a84ccf8651
Simon Hammes <simonhammes@users.noreply.github.com>
2025-12-02 17:40:18 +0100

    Read URL from ENV

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-# TODO: Production URL
 HUGO_BACKEND_URL="https://forms.seatable.com"
+SEATABLE_INTERNAL_API_URL="https://get.seatable.com"
 
 docker compose run --rm \
     -e HUGO_BACKEND_URL="${HUGO_BACKEND_URL}" \
     -e HUGO_ENVIRONMENT=production \
+    -e SEATABLE_INTERNAL_API_URL="${SEATABLE_INTERNAL_API_URL}" \
     hugo bash -c "npm run --prefix /hugo/src/themes/seatable build && hugo --enableGitInfo --source /hugo/src --printPathWarnings --panicOnWarning && npm run --prefix /hugo/src/themes/seatable pagefind:watch"
